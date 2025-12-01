086767ef9d20b0a08bf8cc131f1db8b108f0af2a
Aditya Srivastav <137613253+AdityaSrivastav5@users.noreply.github.com>
2025-12-01 22:59:11 +0530

    fix: Use explicit index.js import for Apollo Client hooks

--- apps/web/src/app/(protected)/employees/page.tsx
+++ apps/web/src/app/(protected)/employees/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from 'react';
-import { useQuery, useMutation, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client/index.js';
 import { PlusIcon, PencilIcon, TrashIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import toast, { Toaster } from 'react-hot-toast';
 
